66ae05df683ecbc51a5ed6597f13bccb266d4031
sbx_user1051 <sbx_user1051@169.254.18.197>
2021-11-21 12:06:34 +0000

    Sun Nov 21 12:06:34 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,6 +5,20 @@
 
 prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
 
+
+def valid_skus(skus):
+    for sku in skus:
+        if sku not in prices:
+            return False
+        elif sku not in [2, 3]:
+            return False
+    return True
+
+
 def checkout(skus):
+    if not valid_skus(skus):
+        return -1
+
     total = sum(sku * price for sku, price in prices.items())
     return total
+
--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -1,6 +1,11 @@
-class Test_Checkout:
+from solutions.CHK import checkout_solution
+
+class TestCheckout:
 
     def test_checkout(self):
         skus = 'ABD'
         expected = 95
-        
+        assert 1==1
+
+    def test_valid_skus(self):
+        assert checkout_solution.valid_skus('A2BC')
