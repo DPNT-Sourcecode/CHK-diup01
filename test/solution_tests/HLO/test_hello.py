70d4935850ed5fb9be71f943a83c17190a1c9bfb
sbx_user1051 <sbx_user1051@169.254.40.181>
2021-11-21 11:25:16 +0000

    Sun Nov 21 11:25:16 UTC 2021

---
+++ test/solution_tests/HLO/test_hello.py
@@ -0,0 +1,7 @@
+from solutions.HLO import hello_solution
+
+class TestHello:
+
+    def test_hello(self):
+        expected = hello_solution.hello('friend')
+        assert expected == 'Hello friend'
