44e1adce28f96481d081cf06279a427ace917bcb
sbx_user1051 <sbx_user1051@169.254.18.197>
2021-11-21 12:49:43 +0000

    Sun Nov 21 12:49:43 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,7 +4,9 @@
 # skus = unicode string
 
 prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
-offer_prices{'A': 130, 'B': 45}
+offer_prices = {'A': 130, 'B': 45}
+offer_multiples = {'A': 3, 'B': 2}
+
 
 def valid_skus(skus):
     for sku in skus:
@@ -17,5 +19,7 @@ def checkout(skus):
     if not valid_skus(skus):
         return -1
     quantities = {sku: skus.count(sku) for sku in skus}
+    normal_quantities = {sku: quantities.get(sku)}
     total = sum(quantities.get(sku, 0) * prices.get(sku, 0) for sku in skus)
     return total
+
