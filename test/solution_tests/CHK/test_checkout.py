b9324d47186b2678e32f5997d72c7c2e26a53cf1
sbx_user1051 <sbx_user1051@169.254.18.197>
2021-11-21 12:40:43 +0000

    Sun Nov 21 12:40:43 UTC 2021

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -3,16 +3,22 @@ from solutions.CHK import checkout_solution
 
 class TestCheckout:
 
-    def test_checkout(self):
-        skus = 'A2BD'
-        expected = 110
+    def test_checkout_no_offer(self):
+        skus = 'ABBDA'
+        expected = 175
+        assert expected == checkout_solution.checkout(skus)
+
+    def test_checkout_with_offer(self):
+        skus = 'DADAABB'
+        expected = 130 + 45 + 30
         assert expected == checkout_solution.checkout(skus)
 
     def test_valid_skus(self):
-        assert checkout_solution.valid_skus('A2BC')
+        assert checkout_solution.valid_skus('ABCBBD')
 
     def test_invalid_skus(self):
         assert checkout_solution.valid_skus('invalid') == False
+
 
 
 
