20563951049b80cbd2ab571365fa133a211f7e01
sbx_user1051 <sbx_user1051@169.254.15.221>
2021-11-22 18:48:30 +0000

    Mon Nov 22 18:48:30 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,11 +1,35 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 
-prices = {'A': [(5, 40), (3, 130 / 3), (1, 50)], 'B': [(2, 45/2), (1, 30)],
-          'C': [(1, 20)], 'D': [(1, 15)], 'E': [(1, 40)], 'F': [(3, 20/3),
-                                                                (1, 10)]}
-freebies = {'E': 'B'}
-freebie_multiples = {'E': 2}
+prices = {'A': [(5, 40), (3, 130 / 3), (1, 50)],
+          'B': [(2, 45 / 2), (1, 30)],
+          'C': [(1, 20)],
+          'D': [(1, 15)],
+          'E': [(1, 40)],
+          'F': [(3, 20 / 3), (1, 10)],
+          'G': [(1, 20)],
+          'H': [(10, 8), (5, 9), (1, 10)],
+          'I': [(1, 35)],
+          'J': [(1, 60)],
+          'K': [(2, 75), (1, 80)],
+          'L': [(1, 90)],
+          'M': [(1, 15)],
+          'N': [(1, 40)],
+          'O': [(1, 10)],
+          'P': [(5, 40), (1, 50)],
+          'Q': [(3, 80/3), (1, 30)],
+          'R': [(1, 50)],
+          'S': [(1, 30)],
+          'T': [(1, 20)],
+          'U': [(4, 120/4), (1, 40)],
+          'V': [(3, 130/3), (2, 45), (1, 50)],
+          'W': [(1, 20)],
+          'X': [(1, 90)],
+          'Y': [(1, 10)],
+          'Z': [(1, 50)]
+          }
+freebies = {'E': 'B', 'N': 'M', 'R': 'Q'}
+freebie_multiples = {'E': 2, 'N': 3, 'R': 3}
 
 
 def valid_skus(skus):
@@ -42,5 +66,7 @@ def checkout(skus):
         return -1
     quantities = {sku: skus.count(sku) for sku in skus}
     quantities = remove_freebies(quantities)
-    total = sum(calc_price(sku, quantity) for sku, quantity in quantities.items())
+    total = sum(
+        calc_price(sku, quantity) for sku, quantity in quantities.items())
     return total
+
--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -1,4 +1,4 @@
-from solutions.CHK import checkout_solution
+from solutions.CHK.checkout_solution import checkout, valid_skus
 
 
 class TestCheckout:
@@ -6,50 +6,105 @@ class TestCheckout:
     def test_checkout_no_offer(self):
         skus = 'ABDA'
         expected = 145
-        assert expected == checkout_solution.checkout(skus)
+        assert expected == checkout(skus)
 
     def test_checkout_with_offer(self):
         skus = 'DADAABB'
         expected = 130 + 45 + 30
-        assert expected == checkout_solution.checkout(skus)
+        assert expected == checkout(skus)
 
     def test_checkout_aaa(self):
         skus = 'AAA'
         expected = 130
-        assert expected == checkout_solution.checkout(skus)
+        assert expected == checkout(skus)
 
     def test_valid_skus(self):
-        assert checkout_solution.valid_skus('ABCBBD')
+        assert valid_skus('ABCBBD')
 
     def test_invalid_skus(self):
-        assert checkout_solution.valid_skus('invalid') == False
+        assert valid_skus('invalid') == False
 
     def test_freebie_offer_all_free(self):
         skus = 'DEEEB'
         expected = 15 + 3 * 40
-        assert expected == checkout_solution.checkout(skus)
+        assert expected == checkout(skus)
 
     def test_freebie_offer_not_all_free(self):
         skus = 'DEEEBB'
         expected = 15 + 3 * 40 + 30
-        assert expected == checkout_solution.checkout(skus)
+        assert expected == checkout(skus)
 
     def test_3_for_2_exact(self):
         skus = 'FFFFFFD'
         expected = 55
-        assert expected == checkout_solution.checkout(skus)
+        assert expected == checkout(skus)
 
     def test_3_for_2_remainder_2(self):
         skus = 'FFFFFD'
         expected = 55
-        assert expected == checkout_solution.checkout(skus)
+        assert expected == checkout(skus)
 
     def test_3_for_2_remainder_1(self):
         skus = 'FFFFD'
         expected = 45
-        assert expected == checkout_solution.checkout(skus)
+        assert expected == checkout(skus)
 
-    def test_combo1(self):
+    def test_h_10(self):
         skus = 10 * 'H'
-        assert 
+        assert 80 == checkout(skus)
+
+    def test_h_5(self):
+        skus = 5 * 'H'
+        assert 45 == checkout(skus)
+
+    def test_h_17(self):
+        skus = 17 * 'H'
+        expected = 80 + 45 + 20
+        assert expected == checkout(skus)
+
+    def test_k_3(self):
+        skus = 3 * 'K'
+        expected = 150 + 80
+        assert expected == checkout(skus)
+
+    def test_n(self):
+        skus = 'NNNNMM'
+        expected = 4 * 40 + 15
+        assert expected == checkout(skus)
+
+    def test_p(self):
+        skus = 7 * 'P'
+        expected = 200 + 100
+        assert expected == checkout(skus)
+
+    def test_q(self):
+        skus = 4 * 'Q'
+        expected = 80 + 30
+        assert expected == checkout(skus)
+
+    def test_r(self):
+        skus = 7 * 'R' + 12 * 'Q'
+        expected = 7 * 50 + 3 * 80 + 30
+        assert expected == checkout(skus)
+
+    def test_u_7(self):
+        skus = 7 * 'U'
+        expected = 6 * 40
+        assert expected == checkout(skus)
+
+    def test_u_9(self):
+        skus = 9 * 'U'
+        expected = 7 * 40
+        assert expected == checkout(skus)
+
+    def test_v_10(self):
+        skus = 10 * 'V'
+        expected = 3 * 130 + 50
+        assert expected == checkout(skus)
+
+    def test_v_17(self):
+        skus = 17 * 'V'
+        expected = 5 * 130 + 90
+        assert expected == checkout(skus)
+
 
