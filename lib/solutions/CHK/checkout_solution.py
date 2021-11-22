e37074cfdf800c8b78ac5f2e4e698021d52213e0
sbx_user1051 <sbx_user1051@169.254.7.21>
2021-11-22 20:00:50 +0000

    Mon Nov 22 20:00:50 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -84,4 +84,3 @@ def checkout(skus):
         calc_price(sku, quantity) for sku, quantity in quantities.items())
     group_total = calc_group_price(group_skus)
     return non_group_total + group_total
-
