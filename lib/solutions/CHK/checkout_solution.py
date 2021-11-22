a5dedc8e2a04336aae524c129f32626cd9d2d8c9
sbx_user1051 <sbx_user1051@169.254.15.221>
2021-11-22 18:14:39 +0000

    Mon Nov 22 18:14:39 UTC 2021

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,7 +1,7 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 
-prices = {'A': [(5, 40), (3, 130 / 3), (1, 50)], 'B': [(2, 45), (1, 30)],
+prices = {'A': [(5, 40), (3, 130 / 3), (1, 50)], 'B': [(2, 45/2), (1, 30)],
           'C': [(1, 20)], 'D': [(1, 15)], 'E': [(1, 40)]}
 freebies = {'E': 'B'}
 freebie_multiples = {'E': 2}
@@ -43,3 +43,4 @@ def checkout(skus):
     quantities = remove_freebies(quantities)
     total = sum(calc_price(sku, quantity) for sku, quantity in quantities.items())
     return total
+
